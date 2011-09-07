e58c1fa192d0b83ab037587e6386062d0a3e1552
Matthieu Wipliez <mwipliez@users.sourceforge.net>
2011-09-07 11:51:03 +0200

    bot handling dates written in natural language.

--- irc_bot.rb
+++ irc_bot.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/env ruby1.9.1
 require 'cinch'
+require 'chronic'
+
+MAX_MSGS = 10000
 
 class History
   include Cinch::Plugin
 
   class HistoryMsg < Struct.new(:user, :time, :channel, :msg)
     def to_s
-      "[#{time.strftime "%H:%M:%S"}] #{channel} #{user}: #{msg}"
+      "[#{time.strftime "%H:%M:%S"}] #{user}: #{msg}"
     end
   end
 
@@ -18,13 +21,31 @@ class History
   match(/(.*)/, :use_prefix => false)
 
   def execute(m)
-    if m.message =~ /!history(.*)/ then
+    if m.message =~ /^!history(.*)/ then
       # send history with private messages to the user depending on the channel
-      m.user.msg "history:"
-      @history.each { |msg| m.user.msg(msg) if (not m.channel?) or msg.channel == m.channel }
+      begin
+        time = Chronic.parse($1, :context => :past)
+        if time == nil then
+          raise "wrong date format"
+        else
+          if m.channel? then
+            m.user.msg "history on the channel " + m.channel + " after " + time.to_s + ":"
+            @history.each { |msg| m.user.msg(msg) if msg.channel == m.channel and msg.time >= time }
+          else
+            m.user.msg "history after " + time.to_s + ":"
+            @history.each { |msg| m.user.msg(msg) if msg.time >= time }
+          end
+        end
+      rescue
+        m.user.msg "please give a date: !history yesterday, !history 2 hours ago, !history 6 in the morning, etc."
+        m.user.msg "more documentation is available at http://chronic.rubyforge.org/files/README.html"
+      end
     elsif m.channel? then
       # only record messages on the channel
       @history << HistoryMsg.new(m.user, Time.now, m.channel, m.message)
+      if @history.size > MAX_MSGS then
+        @history.delete_at(0)
+      end
     end
   end
 
@@ -54,7 +75,7 @@ class Help
   match /help/
 
   def execute(m)
-    m.reply "available commands: !help and !history"
+    m.reply "available commands:\n!help\n!history date (date being written in natural language, e.g. two hours ago)"
   end
 
 end
