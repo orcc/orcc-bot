707ae904ea3c12548d03307a6cc1cf325ae658b6
Antoine Lorence <antoine.lorence@gmail.com>
2012-12-06 13:32:24 +0100

    Daemonize ruby bot using daemons gem

--- README.md
+++ README.md
@@ -8,17 +8,17 @@ Requirements
  * ruby 1.9.1
  * cinch
  * chronic
+ * daemons
 
 On Ubuntu, type with root permissions :
 
     apt-get install ruby1.9.1
-    gem install cinch
-    gem install chronic
+    gem install cinch chronic daemons
 
 How-to use
 ========
 Simply launch the script with command :
 
-    /path/to/irc_bot.rb <channel_without_#>
+    /path/to/irc_bot.rb [start|stop|restart] <channel without the first #>
 
-If you want to launch the script as a service, edit /etc/rc.local to add the command ended with '&'.
+If you want to launch the script at startup, simply add the start command in /etc/rc.local.
--- irc_bot.rb
+++ irc_bot.rb
@@ -1,123 +1,34 @@
 #!/usr/bin/env ruby1.9.1
-require 'cinch'
-require 'chronic'
 
-###############################################################################
-class AutoHello
-  include Cinch::Plugin
+require 'daemons'
 
-  listen_to :join
-  def listen(m)
-    unless m.user.nick.end_with?("-bot")
-      m.reply "hi #{m.user.nick}"
-    end
-  end
-
-end
-
-###############################################################################
-class History
-  MAX_MSGS = 10000
-
-  include Cinch::Plugin
-
-  class HistoryMsg < Struct.new(:user, :time, :channel, :msg)
-    def to_s
-      "[#{time.strftime "%H:%M:%S"}] #{user}: #{msg}"
-    end
-  end
-
-  def initialize(*args)
-    super
-    @history = []
-  end
-
-  match(/(.*)/, :use_prefix => false)
-
-  def execute(m)
-    # only record messages on the channel
-    if m.channel? then
-      @history << HistoryMsg.new(m.user, Time.now, m.channel, m.message)
-      if @history.size > MAX_MSGS then
-        @history.delete_at(0)
-      end
-    end
-
-    # react to messages starting with !history
-    if m.message =~ /^!history(.*)/ then
-      # send history with private messages to the user depending on the channel
-      begin
-        time = Chronic.parse($1, :context => :past)
-        if time == nil then
-          raise "wrong date format"
-        else
-          if m.channel? then
-            m.user.msg "history on the channel " + m.channel + " after " + time.to_s + ":"
-            @history.each { |msg| m.user.msg(msg) if msg.channel == m.channel and msg.time >= time }
-          else
-            m.user.msg "history after " + time.to_s + ":"
-            @history.each { |msg| m.user.msg(msg) if msg.time >= time }
-          end
-        end
-      rescue
-        m.user.msg "please give a date: !history yesterday, !history 2 hours ago, !history 6 in the morning, etc."
-        m.user.msg "more documentation is available at http://chronic.rubyforge.org/files/README.html"
-      end
-    end
-  end
-
-end
-
-###############################################################################
-class Leave
-  include Cinch::Plugin
-  
-  match /leave (.+)/
-
-  def execute(m)
-    if not m.channel? then
-      if m.message =~ /!leave NOW!/ then
-        m.bot.channels.each { |c| c.msg "#{m.user.nick} asked me to leave... bye all!" }
-        m.bot.quit
-      else
-        m.reply "incorrect password. I'm not leaving!"
-      end
-    end
-  end
-
-end
-
-###############################################################################
-class Help
-  include Cinch::Plugin
-  
-  match /help/
-
-  def execute(m)
-    m.reply "available commands:\n!help\n!history date (date being written in natural language, e.g. two hours ago)"
-  end
-
-end
-
-###############################################################################
-# quit unless our script gets two command line arguments
-unless ARGV.length == 1
-  puts "Usage: bobot.rb channel (without the # sign)"
+unless ARGV.length > 0
+  puts "Usage: irc_bot.rb [start|stop|restart] <channel (without the # sign)>"
   exit
 end
 
-channel = ARGV[0]
+currentDir = File.realdirpath(File.dirname(__FILE__))
+logDir = File.join(currentDir, 'logs')
 
-bot = Cinch::Bot.new do
-  configure do |c|
-    c.server = "irc.freenode.org"
-    c.channels = ["#" + channel]
-    c.messages_per_second = 0.5
-    c.nick = channel + "-bot"
-    c.plugins.plugins = [AutoHello, Help, History, Leave]
-    c.verbose = true
-  end
+unless Dir.exists?(logDir) then
+	Dir.mkdir(logDir)
+else
+	for f in Dir.glob(File.join(logDir, "*.{log,output}"))
+		File.delete(f)
+		puts f + " deleted..."
+	end
 end
 
-bot.start
+args = [ARGV.first, '-f', '--'] + ARGV.slice(1, ARGV.length)
 
+options = {
+	:app_name   => "irc_bot",			# app name
+	:ARGV       => args,				# correctly set daemon arguments
+	:backtrace  => true,				# write the last exception if any
+	:monitor    => true,				# create a process to monitor and restart if crashes
+	:dir_mode	=> :normal,				# paths are considered absolutes or relative
+	:log_output => true,				# does the logs should be written ?
+    :log_dir	=> logDir				# dir to write logs files
+}
+
+Daemons.run(File.join(currentDir, 'bot.rb'), options)
